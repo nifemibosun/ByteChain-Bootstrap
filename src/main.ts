1cae579a31e1d9e4df0285a4b2fbe92651e4bdb6
Nifemi Bosun <nifemibosun70@gmail.com>
2026-08-10 15:45:36 +0100

    fix: use dynamic PORT from environment variable in LiteNode startup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import LiteNode from "./litenode.js";
 
 const main = () => {
+    const PORT = Number(process.env.PORT ?? 1000);
     let litenode = new LiteNode();
-    litenode.start(1000);
+    litenode.start(PORT);
 }
 
 main();
